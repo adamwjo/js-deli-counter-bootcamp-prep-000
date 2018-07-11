00ae8deb94ec839276937bf980833d1cb2e972b8
Adam Johnson <adamwjo@gmail.com>
2018-07-11 17:22:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function takeANumber(katzdeliLine, name){
 function nowServing(katzdeliLine){
   if (katzdeliLine.length === 0) {
     return "There is nobody waiting to be served!"
-  } else {
+  }
+  else {
     return `Currently serving ${katzdeliLine.shift()}.`
   }
 }
@@ -15,10 +16,13 @@ function nowServing(katzdeliLine){
 function currentLine(katzdeliLine){
   if (katzdeliLine.length === 0) {
     return "The line is currently empty."
-  } else {
+  }
+  else {
     var katzDeli = []
-    for (let i = 0; i < katzdeliLine.length; i++){
+    for (let i = 0; i < katzdeliLine.length; i++)
+    {
       katzDeli.push(`${i + 1}. ${katzdeliLine[i]}`)
-    }return `The line is currently: ${katzDeli.join(", ")}`
+    }
+    return `The line is currently: ${katzDeli.join(", ")}`
   }
 }
